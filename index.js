f5c52d9f76c42020611edcde4d0a41eda343b73d
feizheng <1290657123@qq.com>
2016-09-01 10:08:07 +0800

    optimize

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 
   'use strict';
 
-  var config = require('./proxy_config.json');
+  var inOptions = require('./proxy_inOptions.json');
   var http = require('http'),
     httpProxy = require('http-proxy');
   var proxy = httpProxy.createProxyServer({});
-  var targetHost = 'http://' + config.target.hostname + ':' + config.target.port;
-  var server = http.createServer(function(req, res) {
-    console.log('Current request url is :',req.url);
-    proxy.web(req, res, {
-      target: targetHost
+
+
+  module.exports = function(inOptions) {
+    var targetHost = 'http://' + inOptions.target.hostname + ':' + inOptions.target.port;
+    var callback = inOptions.callback || function(req, res) {
+      console.log('Current request url is :', req.url);
+    };
+    var server = http.createServer(function(req, res) {
+      callback(req, res);
+      proxy.web(req, res, {
+        target: targetHost
+      });
     });
-  });
-  server.listen(config.proxy.port);
-  console.log("listening on port " + config.proxy.port);
-  
+    server.listen(inOptions.proxy.port);
+    console.log("listening on port " + inOptions.proxy.port);
+  };
+
+
 }())
